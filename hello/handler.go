13eb794a826fc63108b7a7c4d9620019119ddef1
kimesh.gupta <kimesh_gupta@infoobjects.com>
2020-12-21 13:57:06 +0530

    Updating url.

--- hello/handler.go
+++ hello/handler.go
@@ -11,7 +11,7 @@ import (
 func Handle(req handler.Request) (handler.Response, error) {
 	var err error
 
-	message := fmt.Sprintf("Hello world from Kimesh!")
+	message := fmt.Sprintf("Hello India!!!")
 
 	return handler.Response{
 		Body:       []byte(message),
