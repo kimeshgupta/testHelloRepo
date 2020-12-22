ff04916699531a18d3ea4dbd4842bb64fc3c656c
kimeshgupta <75723655+kimeshgupta@users.noreply.github.com>
2020-12-22 12:46:55 +0530

    Update handler.go

--- hello/handler.go
+++ hello/handler.go
@@ -11,7 +11,7 @@ import (
 func Handle(req handler.Request) (handler.Response, error) {
 	var err error
 
-	message := fmt.Sprintf("Hello World!!!")
+	message := fmt.Sprintf("Hello World!!!!!!")
 
 	return handler.Response{
 		Body:       []byte(message),
